6aa57087325c0ce1cf526fc5f3033c36726ea2df
new name <development0264@gmail.com>
2023-03-29 18:39:49 +0530

    update email for sending

--- API/settings.py
+++ API/settings.py
@@ -190,8 +190,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #Email Config
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'bhathgaras@gmail.com'
-EMAIL_HOST_PASSWORD ='kplgloxiosdmcbxb'
+EMAIL_HOST_USER = 'development0265@gmail.com'
+EMAIL_HOST_PASSWORD ='vbjqfmydzabxyxmz'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 # EMAIL_TIMEOUT = 300 # in seconds
