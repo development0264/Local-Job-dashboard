b06a7e0460db86b8f098042096d328bfd3a944f2
new name <development0264@gmail.com>
2023-03-30 12:00:03 +0530

    def service_provider(request): changes

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -12,7 +12,9 @@ urlpatterns = [
     path('userprofile',  csrf_exempt(UserProfileView.as_view()), name='userprofile'),
     path('change_password/<int:pk>', ChangePasswordView.as_view(), name='auth_change_password'),
     # path('logout', LogoutView, name='auth_logout')
-    path('logout', LogoutView.as_view(), name='auth_logout')
+    path('logout', LogoutView.as_view(), name='auth_logout'),
+
+    path('api/service_provider', service_provider,name="bids_list"),
     # path('changepassword/', UserChangePassword.as_view(), name='changepassword'),
     # path('send-reset-password-email/', SendPasswordResetEmail.as_view(),
     #  name='send-reset-password-email'),
